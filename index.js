693ebf710212e307cede0d340fa098bf4f3b66eb
harithagithubri <harithagithubri@example.com>
2022-12-27 10:28:30 +0000

    Congrats Card

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran V</h1>
+      <p className="paragraph">
+        Vishnu institute of computer Education and technology, Bhimavaram
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
